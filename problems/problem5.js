53c6c97cb809739edd48b3540b8131fb74e2a7fa
wualice078 <wualice078@berkeley.edu>
2025-10-23 13:16:03 -0700

    problem 5

--- problems/problem5.js
+++ problems/problem5.js
@@ -2,5 +2,11 @@
 // Return strings containing lowercase "waldo", all in uppercase.
 
 function uppercaseStringsContainingWaldo(arr) {
-  // TODO: your code
+  let a = []
+  for (let val of arr) {
+    if (val.includes("waldo")) {
+      a.push(val.toUpperCase())
+    }
+  }
+  return a
 }
