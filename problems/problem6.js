fca7d8b65916fd90bf73336fbfd6ac5999faf7ce
wualice078 <wualice078@berkeley.edu>
2025-10-23 13:28:52 -0700

    problem 6

--- problems/problem6.js
+++ problems/problem6.js
@@ -2,5 +2,16 @@
 // Combine names[] and ages[] into an array of { name, age }
 
 function namesAndAges(names, ages) {
-  // TODO: your code
+  let a = []
+
+  for (let i in names) {
+    let person = {
+      name : names[i],
+      age : ages[i]
+    };
+    
+    a.push(person)
+  };
+
+  return a
 }
